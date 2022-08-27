3c753aca5314554b87d149524fa6d46e7a82caf8
Iván Décima <94205875+IvanDec0@users.noreply.github.com>
2022-08-27 00:22:30 -0300

    cors

--- src/main/java/com/portfolio/controller/ProjectController.java
+++ src/main/java/com/portfolio/controller/ProjectController.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/project")
-@CrossOrigin(origins = {
+@CrossOrigin(allowedOriginPatterns = {
             "*"
         },
         allowedHeaders = "*",
