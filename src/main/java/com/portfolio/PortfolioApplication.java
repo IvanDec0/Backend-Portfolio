17f6e43247311eb233a4fd3c19da0ff2aea8aa28
Iván Décima <94205875+IvanDec0@users.noreply.github.com>
2022-08-26 22:48:18 -0300

    Update Cors

--- src/main/java/com/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolio/PortfolioApplication.java
@@ -2,10 +2,6 @@ package com.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class PortfolioApplication {
@@ -14,14 +10,6 @@ public class PortfolioApplication {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
 
-    @Bean
-   public WebMvcConfigurer corsConfigurer() {
-      return new WebMvcConfigurerAdapter() {
-         @Override
-         public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**").allowedOrigins("/**");
-         }
-      };
-   }
+   
 
 }
