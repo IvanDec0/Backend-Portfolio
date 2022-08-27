f914de8301721235c85aacf0011f5174719d8319
Iván Décima <94205875+IvanDec0@users.noreply.github.com>
2022-08-26 22:55:47 -0300

    Cors Update

--- src/main/java/com/portfolio/configuration/AppConf.java
+++ src/main/java/com/portfolio/configuration/AppConf.java
@@ -1,16 +1,27 @@
 package com.portfolio.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
-public class AppConf implements WebMvcConfigurer {
+@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+ 		// ...
+	}
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "PUT", "DELETE", "POST");
-    }
-}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+			}
+		};
+	}
